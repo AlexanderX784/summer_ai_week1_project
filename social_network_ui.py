afa47fe68a1000c3676a2376af741bd29014436a
AlexanderX784 <113632232+AlexanderX784@users.noreply.github.com>
2023-07-14 23:32:13 -0400

    Changes made 7/14

--- social_network_ui.py
+++ social_network_ui.py
@@ -3,16 +3,24 @@
 def mainMenu():
     print("")
     print("1. Create a new account")
-    print("2. Manage my account")
-    print("3. Quit")
+    print("2. Login to existing account")
+    print("3. Manage my account")
+    print("4. Quit")
     print("********************************************************")
     return input("Please Choose a number: ")
 
 def manageAccountMenu():
     print("")
     print("1. Edit my details")
-    print("2. Add a friend")
-    print("3. View all my friends")
-    print("4. View all my messages")
-    print("5. <- Go back ")
+    print("2: Check current account")
+    print("3. Add a friend")
+    print("4. View all my friends")
+    print("5. View all my messages")
+    print("6: Remove Friend")
+    print("7: Block Friend")
+    print("8. <- Go back ")
     return input("Please Choose a number: ")
+
+def accountLogin():
+    print('')
+    
