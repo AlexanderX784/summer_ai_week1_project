af2105544f400a1f9d35709cd896a3dc4f3edebf
AlexanderX784 <113632232+AlexanderX784@users.noreply.github.com>
2023-07-15 22:42:28 -0400

    Classes for social network

--- social_network_classes.py
+++ social_network_classes.py
@@ -22,48 +22,80 @@ class SocialNetwork:
         name = input("What would you like to set as your name? ")
         age = input("How old are you? ")
         birthday = input("When is your birthday? ")
-        self.list_of_people.append(Person(name, age, birthday)) #the append function is used to save/upload the inputs into a list (in this case, its uploading to self.list_of_people)
+        account = Person(name, age, birthday)
+        self.list_of_people.append(account) #the append function is used to save/upload the inputs into a list (in this case, its uploading to self.list_of_people)
         pass
-    
-
     #for item in self.list_of_people:
         #if item.id in self.list_of_people:
-
-
+    def login_account(self):
+        if len(self.list_of_people) < 1:
+            print("Account not found")
+        elif len(self.list_of_people) >=1:
+            print(f"The current accounts available to log into are: ")
+            for account in self.list_of_people:
+                num = 1
+                num +=1
+                print(account.id)
+            accountaccessed = input("Which account would you like to use? Pick a number from the list above. The first account in the list is numbered 0. ")
+            accountindex = int(accountaccessed)
+            if accountindex in range(len(self.list_of_people)):
+                loginaccount = self.list_of_people[accountindex]
+                print(f"The account you are trying to log into is: {loginaccount.id}")
+                confirm = input("Are you sure you want to log into this account? y/n ")
+                if confirm == 'y':
+                    print(f"Login successful! The account you are currently logged into is: {loginaccount.id}")
+                if confirm == 'n':
+                    print("Login cancelled.")
+            elif accountindex not in range(len(self.list_of_people)):
+                print("The account you are trying to access is not in the database. Please try again.")
+            elif len(self.list_of_people) < 1:
+                print("There are no accounts that you can log into. Please create a new account and try again.")
 class Person(SocialNetwork):
     def __init__(self, name, age, birthday):
         self.id = name
         self.year = age
         self.birthday = birthday
         self.friendlist = []
+        self.receivedmessages = []
         pass
-    def check_account(self):
-        super().__init__()
-        person1 = Person(self.id, self.year, self.birthday)
-        if person1 not in self.list_of_people:
-            print("Account not found")
-        else:
             #checkforaccount = input("What account would you like to check information for? ")
             #create a program that lets you specify a specific account and then set that account as the variable for current account.
             #currentaccount = 
-            print(f"Your current account is: {self.id}")
+            
             #print(f"Name: {name} Age: {age} Birthday: {birthday} Email: {email}") #modify this so that it prints out the information for that specific account (ccurrentaccount)
-    def add_friend(self, person_object):
-        person_object = input("Please input the username of the friend you would like to add: ")
-        self.friendlist.append(person_object)
+    def add_friend(self, person_object): #previously add_friend(self, person_object)
+            for acctid in person_object.list_of_people:
+                acctnum = 1
+                acctnum +=1
+                print("The users that you can currently add as friends are: ", acctid.id)
+            yesorno = input("Would you like to add someone as a friend? y/n ")
+            if yesorno == 'y':
+                userchoice = input("Please input the number of the user you would like to add from the list above. The first account in the list is numbered 0. ")
+                friendindex = int(userchoice)
+                friend = person_object.list_of_people[friendindex]
+                self.friendlist.append(friend)
+                print("Friend added!")
+            elif yesorno == 'n':
+                print("Cancelled. Returning to previous screen.")
+
+        #print("Friend successfuly added.")
+        #print(self.friendlist)
+
         #implement adding friend. Hint add to self.friendlist
-        pass
-    def loginAccount():
-        super().__init__()
-        thing = input("Please input the name of the account you want to log into: ")
-        #if thing not in self.list_of_people:
-            #print("Account not found")
-        #else:
+            pass
+    # def loginAccount(self):
+    #     super().__init__() 
+    #     if:
+    #         print("Account not found")
+    #     else:
+    #         print("Login successful")
     def check_friends(self):
         print(self.friendlist) 
         pass
     def send_message(self):
         message = input("What message would you like to send? ")
+        print("The users that you can currently send messages to are: ")
+
         #put code here in order to send messages to friends and figure out a way to receive messages
         #implement sending message to friend here
     def receive_message(self):
@@ -79,5 +111,3 @@ class Person(SocialNetwork):
         new_name = input("Enter new name: ")
         self.year = new_age
         self.id = new_name
-
-        
