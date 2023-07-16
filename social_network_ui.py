309cc377908c53d80dcf54c5937237b499f76d8f
AlexanderX784 <113632232+AlexanderX784@users.noreply.github.com>
2023-07-15 22:42:44 -0400

    Setting up the user interface for the social network

--- social_network_ui.py
+++ social_network_ui.py
@@ -2,23 +2,23 @@
 
 def mainMenu():
     print("")
-    print("1. Create a new account")
-    print("2. Login to existing account")
+    print("1. Create a new account") #completed
+    print("2. Login to existing account") #completed
     print("3. Manage my account")
-    print("4. Quit")
+    print("4: Log out of current account")
+    print("5. Quit")
     print("********************************************************")
     return input("Please Choose a number: ")
 
 def manageAccountMenu():
     print("")
     print("1. Edit my details")
-    print("2: Check current account")
-    print("3. Add a friend")
-    print("4. View all my friends")
-    print("5. View all my messages")
-    print("6: Remove Friend")
-    print("7: Block Friend")
-    print("8. <- Go back ")
+    print("2. Add a friend")
+    print("3. View all my friends")
+    print("4. View all my messages")
+    print("5: Remove Friend")
+    print("6: Block Friend")
+    print("7. <- Go back ")
     return input("Please Choose a number: ")
 
 def accountLogin():
